72cf70e2577ccbfc99075e2f46ddc05cc2b0c8c5
empathicqubit <empathicqubit@entan.gl>
2020-12-28 20:02:37 +0000

    Optimize React performance

--- src/stats-webview-content.ts
+++ src/stats-webview-content.ts
@@ -1,5 +1,6 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
+import _sortBy from 'lodash/fp/sortBy';
 
 interface vscode {
     postMessage(message: any): void;
@@ -8,9 +9,51 @@ interface vscode {
 declare const acquireVsCodeApi : () => vscode;
 
 export function _statsWebviewContent() {
+    const r = React.createElement;
+
+    interface spriteData extends ImageData {
+        key: string;
+        blobUrl: string;
+    }
+
+    interface renderProps {
+        runAhead: spriteData | null,
+        current: spriteData | null,
+        sprites: spriteData[],
+    };
+
+    class Main extends React.PureComponent {
+        render() {
+            const props = this.props as renderProps;
+            return r('div', null,
+                [
+                    r('div', { className: 'display-frames' },
+                        !props.runAhead
+                        ? r('h1', 'Loading...')
+                        : r('div', {className: 'next-frame'},
+                            r('h1', null, 'Next Frame'),
+                            r('img', { src: props.runAhead.blobUrl }),
+                        ),
+                        !props.current
+                        ? r('h1', 'Loading...')
+                        : r('div', {className: 'current-frame'},
+                            r('h1', null, 'Current Frame'),
+                            r('img', { src: props.current.blobUrl }),
+                        ),
+                    ),
+                    r('div', { className: 'sprites' }, 
+                        r('h1', null, 'Sprites'),
+                        props.sprites.map(x => 
+                            r('img', { key: x.key, alt: x.key,  src: x.blobUrl })
+                        )
+                    )
+                ]
+            );
+        }
+    }
+
     const vscode = acquireVsCodeApi();
 
-    const r = React.createElement;
     const content = document.querySelector("#content")!;
 
     document.addEventListener('keydown', evt => {
@@ -41,69 +84,72 @@ export function _statsWebviewContent() {
         return false;
     });
 
-    interface renderData {
-        runAheadBlobUrl: string,
-        currentBlobUrl: string,
-        spriteDatas: ImageData[],
+    const data : renderProps = {
+        runAhead: null,
+        current: null,
+        sprites: [],
     };
 
-    const data : renderData = {
-        currentBlobUrl: '',
-        runAheadBlobUrl: '',
-        spriteDatas: [],
-    };
-
-    const blobby = (x: ImageData) => {
-        return URL.createObjectURL(new Blob([new Uint8Array(x.data)], {type: "image/png"}));
-    };
-
-    const render = (data: renderData) => {
-        const out = r('div', null,
-            [
-                r('div', { className: 'display-frames' },
-                    !data.currentBlobUrl && !data.runAheadBlobUrl
-                    ? r('h1', 'Loading...')
-                    : [
-                        r('div', {className: 'next-frame'},
-                            r('h1', null, 'Next Frame'),
-                            r('img', { src: data.runAheadBlobUrl }),
-                        ),
-                        r('div', {className: 'current-frame'},
-                            r('h1', null, 'Current Frame'),
-                            r('img', { src: data.currentBlobUrl }),
-                        ),
-                    ]
-                ),
-                r('div', { className: 'sprites' }, 
-                    r('h1', null, 'Sprites'),
-                    data.spriteDatas.map(x => 
-                        r('img', { key: (x as any).key,  src: blobby(x) })
-                    )
-                )
-            ]
-        );
-
-        ReactDOM.render(out, content);
-    };
-
-    render(data);
+    ReactDOM.render((r as any)(Main, data), content);
 
     window.addEventListener('message', async e => {
         try {
-            if(e.data.current) {
-                data.currentBlobUrl = URL.createObjectURL(new Blob([new Uint8Array(e.data.current.data)], {type: "image/png"}));
+            const msgData : renderProps = e.data;
+            if(msgData.current) {
+                const c = msgData.current;
+                if(!data.current || !data.current.data.every((x, i) => c.data[i] == x)) {
+                    data.current = {
+                        ...c,
+                        blobUrl: URL.createObjectURL(new Blob([new Uint8Array(c.data)], {type: 'image/png' })),
+                    }
+                }
             }
-            if(e.data.runAhead) {
-                data.runAheadBlobUrl = URL.createObjectURL(new Blob([new Uint8Array(e.data.runAhead.data)], {type: "image/png"}));
+            if(msgData.runAhead) {
+                const r = msgData.runAhead;
+                if(!data.runAhead || !data.runAhead.data.every((x, i) => r.data[i] == x)) {
+                    data.runAhead = {
+                        ...r,
+                        blobUrl: URL.createObjectURL(new Blob([new Uint8Array(r.data)], {type: 'image/png' })),
+                    }
+                }
             }
-            if(e.data.sprites && e.data.sprites.length) {
-                data.spriteDatas = e.data.sprites;
+            if(msgData.sprites && msgData.sprites.length) {
+                // Add / Modify
+                for(const sprite of msgData.sprites) {
+                    const newSprite = () : spriteData => ({
+                        ...sprite,
+                        blobUrl: URL.createObjectURL(new Blob([new Uint8Array(sprite.data)], {type: 'image/png' })),
+                    });
+                    let existingIndex = -1;
+                    const existing = data.sprites.find((x, i) => {
+                        existingIndex = i;
+                        return x.key == sprite.key;
+                    });
+                    if(!existing) {
+                        data.sprites = _sortBy(x => x.key, [...data.sprites, newSprite()]);
+                        continue;
+                    }
+
+                    if(!existing.data.every((x, i) => sprite.data[i] == x)) {
+                        data.sprites[existingIndex] = newSprite();
+                    }
+                }
+
+                // Remove
+                for(const s in data.sprites) {
+                    const sprite = data.sprites[s];
+                    const old = msgData.sprites.find(x => x.key == sprite.key);
+                    if(!old) {
+                        data.sprites.splice(parseInt(s), 1);
+                    }
+                }
             }
         }
         catch(e) {
             console.error(e);
         }
 
-        render(data);
+
+        ReactDOM.render((r as any)(Main, data), content);
     });
 }
