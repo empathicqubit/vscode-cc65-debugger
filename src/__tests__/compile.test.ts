ba1ff288006d4b13f3dcc338f9f85521ace1170c
empathicqubit <empathicqubit@entan.gl>
2021-09-13 20:31:32 +0200

    Add 32-bit compiler test.

--- src/__tests__/compile.test.ts
+++ src/__tests__/compile.test.ts
@@ -1,7 +1,6 @@
 import * as assert from 'assert';
 import * as compile from '../compile';
 import * as path from 'path';
-import { EventEmitter } from 'events';
 import * as debugUtils from '../debug-utils';
 import _transform from 'lodash/transform';
 import * as child_process from 'child_process';
@@ -12,46 +11,49 @@ describe('Compile', () => {
     const PROGRAM = BUILD_CWD + '/simple-project.c64';
 
     let pids : number[] = [];
-    const execHandler : debugUtils.ExecHandler = (file, args, opts) => {
-        const promise = new Promise<[number, number]>((res, rej) => {
-            if(args.find(x => x.includes("monitor.js"))) {
-                console.log(args);
-                return [-1, -1];
-            }
-
-            const env : { [key: string]: string | undefined } =
-                _transform(opts.env || {}, (a, c, k) => a[k] = c === null ? undefined : c);
-
-            const proc = child_process.spawn(file, args, {
-                cwd: opts.cwd,
-                stdio: "pipe",
-                shell: true,
-                //shell: __dirname + "/xterm-c",
-                detached: false,
-                env: {
-                    ...process.env,
-                    ...env
+    let execHandler : debugUtils.ExecHandler;
+    beforeEach(() => {
+        execHandler = (file, args, opts) => {
+            const promise = new Promise<[number, number]>((res, rej) => {
+                if(args.find(x => x.includes("monitor.js"))) {
+                    console.log(args);
+                    return [-1, -1];
                 }
+
+                const env : { [key: string]: string | undefined } =
+                    _transform(opts.env || {}, (a, c, k) => a[k] = c === null ? undefined : c);
+
+                const proc = child_process.spawn(file, args, {
+                    cwd: opts.cwd,
+                    stdio: "pipe",
+                    shell: true,
+                    //shell: __dirname + "/xterm-c",
+                    detached: false,
+                    env: {
+                        ...process.env,
+                        ...env
+                    }
+                });
+                proc.stdout.pipe(process.stdout);
+                proc.stderr.pipe(process.stderr);
+                pids.push(proc.pid);
+                const cleanup = (e) => {
+                    proc.stdout.unpipe(process.stdout);
+                    proc.stdout.unpipe(process.stderr);
+                    pids.splice(pids.indexOf(proc.pid), 1);
+                    e && console.error(e)
+                };
+                proc.on('disconnect', cleanup);
+                proc.on('close', cleanup);
+                proc.on('error', cleanup);
+                proc.on('exit', cleanup);
+
+                res([proc.pid, proc.pid]);
             });
-            proc.stdout.pipe(process.stdout);
-            proc.stderr.pipe(process.stderr);
-            pids.push(proc.pid);
-            const cleanup = (e) => {
-                proc.stdout.unpipe(process.stdout);
-                proc.stdout.unpipe(process.stderr);
-                pids.splice(pids.indexOf(proc.pid), 1);
-                e && console.error(e)
-            };
-            proc.on('disconnect', cleanup);
-            proc.on('close', cleanup);
-            proc.on('error', cleanup);
-            proc.on('exit', cleanup);
 
-            res([proc.pid, proc.pid]);
-        });
-
-        return promise;
-    };
+            return promise;
+        };
+    })
 
     afterEach(async () => {
         for(const pid of pids) {
@@ -64,6 +66,25 @@ describe('Compile', () => {
     });
 
     test('Build works', async () => {
+        await compile.clean(BUILD_CWD, execHandler);
+        await compile.build(BUILD_CWD, BUILD_COMMAND, execHandler);
+    });
+
+    test('Build works with 32-bit compiler', async () => {
+        const oldExecHandler = execHandler;
+        execHandler = (file, args, opts) => {
+            let sep = ':';
+            if(process.platform == 'win32') {
+                sep = ';';
+            }
+
+            if(opts && opts.env && opts.env.PATH) {
+                opts.env.PATH = opts.env.PATH.replace(/\/cc65\/bin_(\w+)_\w+/g, '/cc65/bin_$1_x32');
+                console.log(opts.env.PATH);
+            }
+            return oldExecHandler(file, args, opts);
+        }
+        await compile.clean(BUILD_CWD, execHandler);
         await compile.build(BUILD_CWD, BUILD_COMMAND, execHandler);
     });
 
