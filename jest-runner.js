87a9a5a651cef8ff9324ddc28264d2068fd04153
empathicqubit <empathicqubit@entan.gl>
2021-09-26 23:08:48 +0200

    Serial tests again

--- jest-runner.js
+++ jest-runner.js
@@ -3,8 +3,7 @@ const DefaultJestRunner = require('jest-runner').default;
 class SerialJestRunner extends DefaultJestRunner {
   constructor(...args) {
     super(...args);
-    //this.isSerial = !!process.env.GITHUB_ACTIONS;
-    this.isSerial = false;
+    this.isSerial = !!process.env.GITHUB_ACTIONS;
   }
 }
 
