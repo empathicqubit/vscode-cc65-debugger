28b6ec569abac0ae9b2703b778f4f5ca31eb6ff4
empathicqubit <empathicqubit@entan.gl>
2021-09-26 03:02:49 +0200

    Reenable test threading

--- jest-runner.js
+++ jest-runner.js
@@ -3,7 +3,8 @@ const DefaultJestRunner = require('jest-runner').default;
 class SerialJestRunner extends DefaultJestRunner {
   constructor(...args) {
     super(...args);
-    this.isSerial = !!process.env.GITHUB_ACTIONS;
+    //this.isSerial = !!process.env.GITHUB_ACTIONS;
+    this.isSerial = false;
   }
 }
 
