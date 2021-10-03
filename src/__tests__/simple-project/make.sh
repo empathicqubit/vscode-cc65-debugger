b79eb9f0139dbc99556fd0db67969ac2faf9a8fb
empathicqubit <empathicqubit@entan.gl>
2021-10-03 06:46:26 +0200

    Make sure we prefer the newest version of cc65-vice

--- src/__tests__/simple-project/make.sh
+++ src/__tests__/simple-project/make.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "$(readlink -f "$0")" )" > /dev/null && pwd )"
-SHPATH="$(echo "$HOME/.vscode/extensions/entan-gl.cc65-vice-"*"/build.sh")"
+SHPATH="$(ls -t "$HOME/.vscode/extensions/entan-gl.cc65-vice-"*"/build.sh" | head -1)"
 if test -e "$DIR/../../../build.sh"; then
     sh "$DIR/../../../build.sh" make "$@"
 elif test -e "$SHPATH"; then
