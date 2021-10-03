e1f611da897a074c78dbccfefad9a786e79cf52e
empathicqubit <empathicqubit@entan.gl>
2021-10-03 05:15:08 +0200

    Fix the Windows build scripts.

--- build.bat
+++ build.bat
@@ -1,9 +1,23 @@
-pushd %~dp0
+setlocal enabledelayedexpansion enableextensions
+pushd "%~dp0"
 set SCRIPT_DIR=%CD%
 popd
 
 for %%i in (code) do @set CODE=%%~$PATH:i
 
+call :dirname CODE_DIR "!CODE!"
+
 set BUILD=%SCRIPT_DIR%/dist/debug-adapter.js
 set ELECTRON_RUN_AS_NODE=1
-"%CODE%" "%BUILD%" build %*
+"%CODE_DIR%\..\code" "%BUILD%" build %*
+
+goto eof
+
+:dirname <resultVar> <pathVar>
+(
+    set "%~1=%~dp2"
+    exit /b
+)
+
+:eof
+endlocal
