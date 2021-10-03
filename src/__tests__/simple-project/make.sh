a0817503b29917ac0219332f3ff4e3d1325392c1
empathicqubit <empathicqubit@entan.gl>
2021-10-03 06:36:34 +0200

    Remove accidental negation

--- src/__tests__/simple-project/make.sh
+++ src/__tests__/simple-project/make.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "$(readlink -f "$0")" )" > /dev/null && pwd )"
 SHPATH="$(echo "$HOME/.vscode/extensions/entan-gl.cc65-vice-"*"/build.sh")"
-if ! test -e "$DIR/../../../build.sh"; then
+if test -e "$DIR/../../../build.sh"; then
     sh "$DIR/../../../build.sh" make "$@"
 elif test -e "$SHPATH"; then
     sh "$SHPATH" make "$@"
