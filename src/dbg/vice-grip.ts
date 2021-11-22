7b6cc479184afee3bbec197678ca4f24a123c4ca
empathicqubit <empathicqubit@entan.gl>
2021-11-22 18:10:27 +0100

    fix: Windows does not need extra double quotes. Thanks @nickbungus !

--- src/dbg/vice-grip.ts
+++ src/dbg/vice-grip.ts
@@ -25,7 +25,6 @@ export class ViceGrip extends AbstractGrip {
         let q = "";
         let sep = ":";
         if(process.platform == "win32") {
-            q = '"';
             sep = ';';
         }
 
