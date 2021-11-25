6b305b2b7ac061f1ef6abb504ad17ce280005d6e
empathicqubit <empathicqubit@entan.gl>
2021-11-25 06:57:55 +0100

    Update Mono version

--- xvfb-wrapper.sh
+++ xvfb-wrapper.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-{ while ! test -e "$(dirname "${@:2:1}")/libMesenCore.dll" ; do "$@" ; done ; } || echo 'merp' ; xeyes & "$@" && kill -9 $(pidof xeyes) || ls -R "$()"
+xeyes & "$@" && kill -9 $(pidof xeyes)
