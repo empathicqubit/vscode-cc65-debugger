f84e812ea1b89c66188db7643d48705d5d962e24
empathicqubit <empathicqubit@entan.gl>
2021-11-25 02:38:04 +0100

    Different arrangement

--- xvfb-wrapper.sh
+++ xvfb-wrapper.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-xeyes & { while ! test -e "$(dirname "${@:2:1}")/libMesenCore.dll" ; do "$@" ; done ; } && "$@" && kill -9 $(pidof xeyes) || ls -R "$()"
+{ while ! test -e "$(dirname "${@:2:1}")/libMesenCore.dll" ; do "$@" ; done ; } || echo 'merp' ; xeyes & "$@" && kill -9 $(pidof xeyes) || ls -R "$()"
