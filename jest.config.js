36f7f9a3e69f20c5281feae710a34afbdd44c113
empathicqubit <empathicqubit@entan.gl>
2022-02-19 06:34:57 +0100

    Jest junit

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const idx = process.argv.indexOf("--outputFile");
+const outputName = process.argv[idx + 1].replace(".json", ".xml");
 module.exports = {
     "testTimeout": 35000,
+    "reporters": ["default", ["jest-junit", { outputName }]],
     "runner": "<rootDir>/jest-runner.js",
     "roots": [
         "<rootDir>/src"
