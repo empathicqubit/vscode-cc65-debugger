422978980d4f54b1a5febd0a03eb5d2afb9b4908
empathicqubit <empathicqubit@entan.gl>
2022-02-19 04:46:29 +0100

    I love Microsoft

--- build.sh
+++ build.sh
@@ -1,10 +1,23 @@
-DIR="$( cd "$( dirname "$(readlink -f "$0")" )" > /dev/null && pwd )"
+READLINK=$(which greadlink readlink | head -1)
+DIR="$( cd "$( dirname "$("$READLINK" -f "$0")" )" > /dev/null && pwd )"
 echo "$DIR"
 BUILD="$DIR/dist/debug-adapter.js"
 CODE="/snap/code/current/usr/share/code/code"
 
-if ! test -e "$CODE" ; then
+if test "$(uname)" == "Darwin" ; then
+    APPPATH="/Applications/Visual Studio Code.app"
+    if ! test -e "$APPPATH" ; then
+        APPPATH="$(ps ax -o command | grep -v '^grep' | grep 'Visual Studio Code.app' | awk -F'/Contents/' '{print $1}' | uniq)"
+    fi
+    CODE="$APPPATH/Contents/MacOS/Electron"
+elif ! test -e "$CODE" ; then
     CODE="$(dirname "$(dirname "$(which code)")")/share/code/code"
 fi
 
-ELECTRON_RUN_AS_NODE=1 "$CODE" "$BUILD" build "$@"
+# MS introduced this freaking command line switch 1.62 Oct 2021
+STUPID_SWITCH=()
+if ELECTRON_RUN_AS_NODE=1 "$CODE" -e "process.exit(0)" --ms-enable-electron-run-as-node ; then
+    STUPID_SWITCH=("--ms-enable-electron-run-as-node")
+fi
+
+ELECTRON_RUN_AS_NODE=1 "$CODE" "$BUILD" "${STUPID_SWITCH[@]}" build "$@"
