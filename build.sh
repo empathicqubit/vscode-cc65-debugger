5dff50ae99037c01f910cf0a335f4bf838e19325
empathicqubit <empathicqubit@entan.gl>
2022-02-20 11:26:26 +0100

    fix: Fix project build scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-READLINK=$(which greadlink readlink | head -1)
+#! /bin/sh
+READLINK="$(which greadlink readlink | head -1)"
 DIR="$( cd "$( dirname "$("$READLINK" -f "$0")" )" > /dev/null && pwd )"
 echo "$DIR"
 BUILD="$DIR/dist/debug-adapter.js"
 CODE="/snap/code/current/usr/share/code/code"
 
-if test "$(uname)" == "Darwin" ; then
+if test "$(uname)" = "Darwin" ; then
     APPPATH="/Applications/Visual Studio Code.app"
     if ! test -e "$APPPATH" ; then
         APPPATH="$(ps ax -o command | grep -v '^grep' | grep 'Visual Studio Code.app' | awk -F'/Contents/' '{print $1}' | uniq)"
@@ -15,9 +16,9 @@ elif ! test -e "$CODE" ; then
 fi
 
 # MS introduced this freaking command line switch 1.62 Oct 2021
-STUPID_SWITCH=()
+STUPID_SWITCH=""
 if ELECTRON_RUN_AS_NODE=1 "$CODE" -e "process.exit(0)" --ms-enable-electron-run-as-node ; then
-    STUPID_SWITCH=("--ms-enable-electron-run-as-node")
+    STUPID_SWITCH="--ms-enable-electron-run-as-node"
 fi
 
-ELECTRON_RUN_AS_NODE=1 "$CODE" "$BUILD" "${STUPID_SWITCH[@]}" build "$@"
+ELECTRON_RUN_AS_NODE=1 "$CODE" "$BUILD" $STUPID_SWITCH build "$@"
--- src/__tests__/simple-project/mk.sh
+++ src/__tests__/simple-project/mk.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 READLINK=$(which greadlink readlink | head -1)
 DIR="$( cd "$( dirname "$("$READLINK" -f "$0")" )" > /dev/null && pwd )"
 SHPATH="$(ls -t "$HOME/.vscode/extensions/entan-gl.cc65-vice-"*"/build.sh" | head -1)"
