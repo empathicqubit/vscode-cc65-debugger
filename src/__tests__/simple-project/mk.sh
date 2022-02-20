560cf550ebd917a2227fb73fd233de3b4d0b0653
empathicqubit <empathicqubit@entan.gl>
2022-02-20 18:41:28 +0100

    Don't try to find extension path if we're in the extension source.

--- src/__tests__/simple-project/mk.sh
+++ src/__tests__/simple-project/mk.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
-READLINK=$(which greadlink readlink | head -1)
+READLINK="$(which greadlink readlink | head -1)"
 DIR="$( cd "$( dirname "$("$READLINK" -f "$0")" )" > /dev/null && pwd )"
-SHPATH="$(ls -t "$HOME/.vscode/extensions/entan-gl.cc65-vice-"*"/build.sh" | head -1)"
 if test -e "$DIR/../../../build.sh"; then
     sh "$DIR/../../../build.sh" make "$@"
-elif test -e "$SHPATH"; then
-    sh "$SHPATH" make "$@"
 else
-    make "$@"
+    SHPATH="$(ls -t "$HOME/.vscode/extensions/entan-gl.cc65-vice-"*"/build.sh" | head -1)"
+    if test -e "$SHPATH"; then
+        sh "$SHPATH" make "$@"
+    else
+        make "$@"
+    fi
 fi
