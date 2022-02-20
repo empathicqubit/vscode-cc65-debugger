335132ee49844b6d52eb73fb757885dbb1086093
empathicqubit <empathicqubit@entan.gl>
2022-02-20 23:15:51 +0100

    Readlink on Mac

--- src/__tests__/c-project-template/mk.sh
+++ src/__tests__/c-project-template/mk.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 READLINK="$(which greadlink readlink | head -1)"
-DIR="$( cd "$( dirname "$("$READLINK" -f "$0")" )" > /dev/null && pwd )"
+DIR="$( cd "$( dirname "$("$READLINK" -f "$0" 2>/dev/null || "$READLINK" "$0" 2>/dev/null)" )" > /dev/null && pwd )"
 if test -e "$DIR/../../../build.sh"; then
     sh "$DIR/../../../build.sh" make "$@"
 else
