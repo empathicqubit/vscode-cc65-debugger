09102eb9d4cc6476829bd9796d2e1d454598f8c8
empathicqubit <empathicqubit@entan.gl>
2022-10-30 10:54:39 +0100

    feat: Add devcontainer.json for easier development in Codespaces

--- docker/build-vice.sh
+++ docker/build-vice.sh
@@ -2,13 +2,13 @@
 set -e
 
 version=$1
-shift
 folr=$2
+shift
 shift
 
 mkdir -p /vices/sources
 cd /vices/sources
-curl -L https://downloads.sourceforge.net/project/vice-emu/releases/vice-$each.tar.gz > /vices/sources/vice-$version.tar.gz
+curl -L https://downloads.sourceforge.net/project/vice-emu/releases/vice-$version.tar.gz > /vices/sources/vice-$version.tar.gz
 tar xvf /vices/sources/vice-$version.tar.gz
 mkdir -p /vices/$folr/vice-$version
 cd /vices/$folr/vice-$version
