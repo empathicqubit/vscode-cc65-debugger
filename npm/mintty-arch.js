d03423e0a12e5e47e0887ca75ce074857e5de9fd
empathicqubit <empathicqubit@entan.gl>
2022-11-08 00:37:14 +0100

    Rearrange original mintty script

--- npm/mintty-arch.js
+++ npm/mintty-arch.js
@@ -38,7 +38,7 @@ const sevenzx = (src, ...files) => {
         return;
     }
 
-    shx.exec(zip7 + ' x -y "' + src + '" ' + files.map(x => `"${x}"`).join(''));
+    shx.exec(zip7 + ' x -y "' + src + '" ' + files.map(x => `"${x}"`).join(' '));
 }
 
 /**
@@ -50,7 +50,7 @@ const untar = (src, ...files) => {
         return;
     }
 
-    shx.exec('tar -x -v -f "' + src + '" ' + files.map(x => `"${x}"`).join(''))
+    shx.exec('tar -x -v -f "' + src + '" ' + files.map(x => `"${x}"`).join(' '))
 }
 
 /**
@@ -68,29 +68,15 @@ const unzst = (src) => {
 try {
     console.time('mintty');
 
-    if(process.platform == 'win32') {
-        downloadFile('https://github.com/facebook/zstd/releases/download/v1.5.2/zstd-v1.5.2-win32.zip', 'obj/mintty/zstd.zip');
-        shx.cd('obj/mintty');
-        sevenzx('./zstd.zip', 'zstd-v1.5.2-win32/zstd.exe');
-        process.env.PATH = process.env.PATH + ";" + __dirname + "/../obj/mintty/zstd-v1.5.2-win32";
-        shx.cd('../..');
-    }
-
     if(arch == 'win32_x64') {
-        downloadFile('https://repo.msys2.org/msys/x86_64/msys2-runtime-3.3.6-3-x86_64.pkg.tar.zst', `obj/mintty/${arch}/msys2-runtime.pkg.tar.zst`);
-        downloadFile('https://mirror.msys2.org/msys/x86_64/mintty-1~3.6.1-2-x86_64.pkg.tar.zst', `obj/mintty/${arch}/mintty.pkg.tar.zst`);
+        downloadFile('https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/PortableGit-2.33.0.2-64-bit.7z.exe', `obj/mintty/${arch}/git.7z.exe`);
     }
     else if(arch == 'win32_x32') {
-        downloadFile('https://repo.msys2.org/msys/i686/msys2-runtime-3.2.0-14-i686.pkg.tar.zst', `obj/mintty/${arch}/msys2-runtime.pkg.tar.zst`);
-        downloadFile('https://mirror.msys2.org/msys/i686/mintty-1~3.5.0-1-i686.pkg.tar.zst', `obj/mintty/${arch}/mintty.pkg.tar.zst`);
+        downloadFile('https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/PortableGit-2.33.0.2-32-bit.7z.exe', `obj/mintty/${arch}/git.7z.exe`);
     }
 
     shx.cd('obj/mintty/' + arch);
-    unzst('./mintty.pkg.tar.zst');
-    unzst('./msys2-runtime.pkg.tar.zst');
-
-    untar('./mintty.pkg.tar', 'usr/bin/mintty.exe');
-    untar('./msys2-runtime.pkg.tar', 'usr/bin/msys-2.0.dll');
+    sevenzx('./git.7z.exe', 'usr/bin/mintty.exe', 'usr/bin/msys-2.0.dll');
 
     const distDir = '../../../dist/mintty/bin_' + arch;
     maybeMkdir(distDir);
