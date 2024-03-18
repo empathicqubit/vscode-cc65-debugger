7fe8b12dbe8406d035d1678ae13a90a6f7ab7858
empathicqubit <empathicqubit@entan.gl>
2024-03-18 02:01:14 +0100

    simplify test project makefiles since they aren't for users anyway

--- src/__tests__/asm-project/mk.sh
+++ src/__tests__/asm-project/mk.sh
@@ -4,10 +4,5 @@ DIR="$( cd "$( dirname "$( "$READLINK" -f "$0" )" )" > /dev/null && pwd )"
 if test -e "$DIR/../../../build.sh"; then
     sh "$DIR/../../../build.sh" make "$@"
 else
-    SHPATH="$(ls -t "$HOME/.vscode/extensions/entan-gl.cc65-vice-"*"/build.sh" | head -1)"
-    if test -e "$SHPATH"; then
-        sh "$SHPATH" make "$@"
-    else
-        make "$@"
-    fi
+    make "$@"
 fi
