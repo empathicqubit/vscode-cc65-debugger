04c14f3afec1d49950d85502681196384cb81d41
empathicqubit <empathicqubit@entan.gl>
2025-02-08 20:59:52 +0100

    fix: Newer versions of vscode don't do anything with the electron switch

--- src/dbg/debug-adapter.ts
+++ src/dbg/debug-adapter.ts
@@ -1,17 +1,17 @@
 /**!
  * @license vscode-cc65-debugger 2024
  * Copyright (c) 2024, EmpathicQubit
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is furnished
  * to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -33,7 +33,7 @@ const build = async() => {
     }, debugUtils.DEFAULT_HEADLESS_EXEC_HANDLER(buf => process.stdout.write(buf), buf => process.stderr.write(buf)));
 }
 
-const [,,command,...rest] = process.argv;
+const [,,command,...rest] = process.argv.filter(x => x !== '--ms-enable-electron-run-as-node');
 if(command == 'build') {
     build().then(() => {
         process.exit(0);
